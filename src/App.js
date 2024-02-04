dc7bff94d889073c6a58d2f9021885cdf2a83e69
Jegal MinHyuk <104841568+jgmh99@users.noreply.github.com>
2024-02-04 16:55:11 +0900

    파이어베이스 api안보이게

--- src/App.js
+++ src/App.js
@@ -27,13 +27,7 @@ import Grid from '@mui/material/Grid';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGzWy1GO-oYHD5E97PawlRZI_iODhoT1I",
-  authDomain: "todo-list-1dc5f.firebaseapp.com",
-  projectId: "todo-list-1dc5f",
-  storageBucket: "todo-list-1dc5f.appspot.com",
-  messagingSenderId: "117786700415",
-  appId: "1:117786700415:web:1f01cdb9a610acb7616d4a",
-  measurementId: "G-ZEPNV4T50S"
+  
 };
 
 const app = initializeApp(firebaseConfig);
@@ -294,7 +288,7 @@ function App() {
                         onRemoveClick={onRemoveClick} />
                     </div>
                   </Grid>
-                  
+
                   {/* 그래프 */}
                   <Grid item xs={3}>
                     <Typography variant="h6" component="div">
